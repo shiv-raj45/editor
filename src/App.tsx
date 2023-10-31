03f2a42180889dd6b8dcfd56ce199adab1460e3b
shiva-raj <shivarajchimouriya@bottle.com.np>
2023-10-31 10:37:05 +0545

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+
+import "./App.css";
+import FroalaEditor from "react-froala-wysiwyg";
+import "froala-editor/css/froala_style.min.css";
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/js/plugins/video.min.js"
+import "froala-editor/js/plugins/video.min.js"
+import "froala-editor/js/plugins/image.min.js"
+import "froala-editor/js/plugins/draggable.min.js"
+import "froala-editor/js/plugins/colors.min.js"
+import "froala-editor/js/plugins/files_manager.min.js"
+import "froala-editor/js/plugins/quote.min.js"
+import "froala-editor/js/plugins/word_paste.min.js"
+import "froala-editor/js/plugins/print.min.js"
+import "froala-editor/js/plugins/font_size.min.js"
+import "froala-editor/js/plugins/code_view.min.js"
+import "froala-editor/js/plugins/code_beautifier.min.js"
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <div id="editor"></div>
+      <FroalaEditor tag="textarea" config={{
+        
+      }} />
+    </>
+  );
+}
+
+export default App;
